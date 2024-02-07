ec54aa94d7825ee43834d23380f947d461e4a713
Crowdbotics <team@crowdbotics.com>
2024-02-07 15:25:52 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "skillshare-lite-46057",
+  url: "https://skillshare-lite-46057.botics.co",
+  api: "https://skillshare-lite-46057.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
